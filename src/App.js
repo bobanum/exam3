e1ac46862faf05a1f386599cacde47c6f3f61293
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2019-09-26 07:07:24 -0400

    Solutionnaire

--- src/App.js
+++ src/App.js
@@ -7,10 +7,56 @@ export default class App {
 	 * Méthode principale. Sera typiquement appelée après le chargement de la page.
 	 */
 	static main() {
-		console.log("Je suis prêt");
-		document.getElementById("app").innerHTML = "La page est chargée";
+		this.app = document.getElementById("app");
+		this.chargerJson("http://prof-tim.cstj.qc.ca/martinboudreau/proxy.php/http://dnd5eapi.co/api/monsters").then(data => {
+			this.app.appendChild(this.liste(data.results))
+		});
 	}
-	/**
+	static liste(donnees) {
+		var resultat = document.createElement("select");
+		resultat.setAttribute("name", "monsters");
+		resultat.setAttribute("id", "monsters");
+		donnees.map(donnee => {
+			var option = resultat.appendChild(document.createElement("option"));
+			option.setAttribute("value", donnee.url);
+			option.innerHTML = donnee.name;
+		});
+		resultat.setAttribute("onchange", "window.open(this.value);");
+		return resultat;
+	}
+	static chargerJson(url) {
+		return new Promise(resolve => {
+			var xhr = new XMLHttpRequest();
+			xhr.open("get", url);
+			xhr.responseType = "json";
+			xhr.addEventListener("load", e => {
+				resolve(xhr.response, xhr);
+			});
+			xhr.send();
+		});
+	}
+	static chargerXml(url) {
+		return new Promise(resolve => {
+			var xhr = new XMLHttpRequest();
+			xhr.open("get", url);
+			xhr.responseType = "document";
+			xhr.addEventListener("load", e => {
+				resolve(xhr.response, xhr);
+			});
+			xhr.send();
+		});
+	}
+	static chargerTxt(url) {
+		return new Promise(resolve => {
+			var xhr = new XMLHttpRequest();
+			xhr.open("get", url);
+			xhr.responseType = "text";
+			xhr.addEventListener("load", e => {
+				resolve(xhr.responseText, xhr);
+			});
+			xhr.send();
+		});
+	}	/**
 	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
 	 * @returns {Promise} La promesse qui sera résolue après chargement
 	 */
